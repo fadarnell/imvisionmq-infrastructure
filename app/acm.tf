c0994f4cd5a5a862585c249a2a7e7c23d5dcdacf
Dawid Orzeł <dawid.orzel@miquido.com>
2023-09-29 11:14:14 +0200

    feat(infrastructure): delete dynamic envs

--- app/acm.tf
+++ app/acm.tf
@@ -13,19 +13,3 @@ module "acm_alb" {
   wait_for_certificate_issued = true
   tags                        = var.tags
 }
-
-module "acm_dynamic" {
-  source = "../modules/terraform-acm-request-certificate-3.0.6"
-  count  = var.environment == "dev" ? 1 : 0
-  providers = {
-    aws.acm = aws
-    aws.dns = aws
-  }
-
-  domain_name                 = "dynamic.${local.app_domain}"
-  ttl                         = "300"
-  subject_alternative_names   = ["*.dynamic.${local.app_domain}"]
-  hosted_zone_id              = aws_route53_zone.app_domain.zone_id
-  wait_for_certificate_issued = true
-  tags                        = var.tags
-}
--- app/alb.tf
+++ app/alb.tf
@@ -13,6 +13,5 @@ module "alb" {
   access_logs_s3_bucket_force_destroy = false
   access_logs_enabled                 = true
   acm_certificate_arn                 = module.acm_alb.arn
-  additional_certs                    = var.environment == "dev" ? [module.acm_dynamic.arn] : []
   idle_timeout                        = 600
 }
